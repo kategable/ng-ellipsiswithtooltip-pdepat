5ff267360a44b31756ba91996bf2a819ffe3bc73
katesky <kateskytech@gmail.com>
2021-03-30 22:18:18 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ng-ellipsiswithtooltip-pdepat
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ng-ellipsiswithtooltip-pdepat)
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = 'USER';
+  longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+}
